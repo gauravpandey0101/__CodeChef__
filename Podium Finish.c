d1f8bfc50b4aa02ec6b70d459e68580783726af4
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2022-10-11 15:46:54 +0530

    Update Podium Finish.c

--- Podium Finish.c
+++ Podium Finish.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 int main()
 {
-	// your code goes here
 	int cases,a,b,i;
 	scanf("%d" , &cases);
-    for(i=0; i<cases; i++)
-    {
-        scanf("%d%d" , &a,&b);
-        printf("%d\n" , (a+b));
-    }
+        for(i=0; i<cases; i++)
+        {
+        	scanf("%d%d" , &a,&b);
+        	printf("%d\n" , (a+b));
+    	}
 	return 0;
 }
